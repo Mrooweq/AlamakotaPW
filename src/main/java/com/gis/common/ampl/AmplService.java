0b8653f23a2136289c4bcaa89290d832ea4d6780
Adam <apacz_0@o2.pl>
2018-11-10 23:02:19 +0100

    ampl cz. 2

--- src/main/java/com/gis/common/ampl/AmplService.java
+++ src/main/java/com/gis/common/ampl/AmplService.java
@@ -1,54 +1,129 @@
 package com.gis.common.ampl;
 
+import com.gis.algorithm.Algorithm;
 import com.gis.algorithm.GraphGenerator;
-import com.gis.graph.Graph;
-import com.gis.graph.Vertex;
-import com.gis.graph.Wrapper;
+import com.gis.common.exception.NoPathException;
+import com.gis.graph.*;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class AmplService {
 
     private static final Pattern PATTERN = Pattern.compile("^(\\d)\\s(\\d)\\s{3}\\d$");
 
     public static void main(String[] args) {
-        Wrapper wrapper = GraphGenerator.veryBigGraph();
+        Wrapper wrapper = GraphGenerator.smallGraph3();
 
-        optimize(wrapper);
+        try {
+            PathWrapper algoPaths = Algorithm.getPaths(wrapper.getGraph(), wrapper.getFrom(), wrapper.getTo());
+            PathWrapper2 optPaths = optimize(wrapper);
+
+            System.out.println();
+
+        } catch (NoPathException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static List<Integer> optimize(Wrapper wrapper){
-        List<Integer> path = new ArrayList<>();
+    public static PathWrapper2 optimize(Wrapper wrapper){
         try {
             generateDataFile(wrapper);
-            path = getOptimalPathByOptimization();
-
-            System.out.println("size: " + path.size());
+            return getOptimalPathByOptimization();
         }
         catch (Exception e){
             e.printStackTrace();
+            throw new RuntimeException("Nie udalo sie przeprowadzic optymalizacji");
         }
-        return path;
     }
 
     private static void generateDataFile(Wrapper wrapper) {
+        String setInter = "set INTER := ";
+        String entr = "param entr := ";
+        String exit = "param exit := ";
+        String param = "param:  ROADS:  flow := ";
+
         Graph graph = wrapper.getGraph();
-        Vertex from = wrapper.getFrom();
-        Vertex to = wrapper.getTo();
+        String from = String.valueOf(wrapper.getFrom().getId());
+        String to = String.valueOf(wrapper.getTo().getId());
+
+        String setInterInput = graph.getVertices().stream()
+                .map(Vertex::getId)
+                .map(String::valueOf)
+                .collect(Collectors.joining(" "));
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(setInter);
+        sb.append(setInterInput);
+        sb.append(";");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(entr);
+        sb.append(from);
+        sb.append(";");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(exit);
+        sb.append(to);
+        sb.append(";");
+        sb.append(System.getProperty("line.separator"));
+        sb.append(param);
+        sb.append(System.getProperty("line.separator"));
+
+        for (Iterator<Edge> iterator = graph.getEdges().iterator(); iterator.hasNext(); ) {
+            Edge edge = iterator.next();
+            String source = String.valueOf(graph.getSource(edge).getId());
+            String dest = String.valueOf(graph.getDest(edge).getId());
+
+            String line = "\t" + "\t" + source + " " + dest + "\t" + "\t" + String.valueOf(edge.getFlow());
+
+            if(iterator.hasNext()){
+                line = line + ",";
+            }
+            else {
+                line = line + ";";
+            }
+
+            sb.append(line);
+            sb.append(System.getProperty("line.separator"));
+        }
+
+        try {
+            Files.write(Paths.get("data.dat"), sb.toString().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    private static List<Integer> getOptimalPathByOptimization() throws Exception  {
-        String start = "C:\\Users\\Lenovo\\amplide\\ampl.exe";
-        String commands = "model C:\\Users\\Lenovo\\AlamakotaPW\\network.mod;\n" +
+    private static PathWrapper2 getOptimalPathByOptimization() throws Exception  {
+        String minCommand = "model C:\\Users\\Lenovo\\AlamakotaPW\\networkMin.mod;\n" +
                 "data C:\\Users\\Lenovo\\AlamakotaPW\\data.dat;\n" +
                 "option solver knitro;\n" +
                 "solve;\n" +
                 "option omit_zero_rows 1;\n" +
                 "display Use;\n" +
                 "reset;\n";
+
+        String maxCommand = "model C:\\Users\\Lenovo\\AlamakotaPW\\networkMax.mod;\n" +
+                "data C:\\Users\\Lenovo\\AlamakotaPW\\data.dat;\n" +
+                "option solver knitro;\n" +
+                "solve;\n" +
+                "option omit_zero_rows 1;\n" +
+                "display Use;\n" +
+                "reset;\n";
+
+        List<Integer> minPathByOptimization = getOptPathByOptimization(minCommand);
+        List<Integer> maxPathByOptimization = getOptPathByOptimization(maxCommand);
+
+        return new PathWrapper2(minPathByOptimization, maxPathByOptimization);
+    }
+
+    private static List<Integer> getOptPathByOptimization(String command) throws Exception  {
+        String start = "C:\\Users\\Lenovo\\amplide\\ampl.exe";
 
         ProcessBuilder builder = new ProcessBuilder(start);
 
@@ -62,7 +137,7 @@ public class AmplService {
 
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
 
-        writer.write(commands);
+        writer.write(command);
         writer.flush();
         writer.close();
 
